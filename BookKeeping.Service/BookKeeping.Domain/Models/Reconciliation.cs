0c3633c0900ff2a60926e90f1d5d3b3766d94cb9
Minhaz <minhaz1217@gmail.com>
2022-09-04 23:35:08 +0600

    db created with entity framework

---
+++ BookKeeping.Service/BookKeeping.Domain/Models/Reconciliation.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookKeeping.Service.API.Models
+{
+    public class Reconciliation
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; } = string.Empty;
+        
+        [Required]
+        public ReconciliationType Type { get; set; }
+    }
+
+    public enum ReconciliationType
+    {
+        Expense = 5,
+        Income = 10
+    }
+}
