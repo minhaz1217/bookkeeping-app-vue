3c577613d11c19704b053a71abd98b37852c802e
Minhaz <minhaz1217@gmail.com>
2022-09-06 01:59:35 +0600

    handled fetch error

--- BookKeeping.Web/src/services/ReconciliationService.ts
+++ BookKeeping.Web/src/services/ReconciliationService.ts
@@ -3,20 +3,46 @@ import { YearWiseCalculatedData } from "@/shared/models/YearWIseCalculatedData";
 
 export const GetReconciliationDataByYear = async (year: number): Promise<YearWiseCalculatedData> => {
     const url = BACKEND_URL + "/api/Reconciliation/get-reconciliations-by-year?year=" + year;
-    const res = await fetch(url);
-    const data = await res.json();
+
+    let data: any = null;
+    const res =
+            (await fetch(url, {
+                method: "GET"
+            })
+            .then(async response => {
+                if (!response.ok) {
+                    alert("Error: " + response.status);
+                } else {
+                    data = await response.json();
+                }
+            })
+            .catch(error => {
+                alert(error);
+            }));
     let output = new YearWiseCalculatedData(data);
     return output;
 }
 
-export const SaveReconciliationData = async (data : YearWiseCalculatedData)=> {
+export const SaveReconciliationData = async (data: YearWiseCalculatedData) => {
     const url = BACKEND_URL + "/api/Reconciliation/save-reconciliation-data";
+    
+    let output: any = null;
     const res = await fetch(url, {
-        method : "POST",
-        headers: {
-            "Content-type": "application/json",
-        },
-        body: JSON.stringify(data)
+            method: "POST",
+            headers: {
+                "Content-type": "application/json",
+            },
+            body: JSON.stringify(data)
+        })
+        .then(async response => {
+            if (!response.ok) {
+                alert("Error: " + response.status);
+            } else {
+                output = await response.json();
+            }
+        })
+        .catch(error => {
+        alert(error);
     });
-    return res;
+    return output;
 }
